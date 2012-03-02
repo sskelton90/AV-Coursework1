1579bb57d83655916a5ee7fdd400d50406fe308f
Andy <zegg90@gmail.com>
2012-03-02 16:57:21 +0000

    Added return value to ComputeMHI, added ComputeDescriptor file

---
+++ ComputeDescriptor.m
@@ -0,0 +1,9 @@
+function [ output_args ] = ComputeDescriptor( directory, start, finish )
+%COMPUTEDESCRIPTOR Summary of this function goes here
+%   Detailed explanation goes here
+
+mhi = ComputeMHI(directory, start, finish);
+
+
+end
+
