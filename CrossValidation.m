1200ea26cede068e6f3f9418df0bc3ff990d51f8
Andy <zegg90@gmail.com>
2012-03-08 16:12:46 +0000

    Cross validation now give the average result

--- CrossValidation.m
+++ CrossValidation.m
@@ -1,6 +1,6 @@
 N = 24; %Sets of data
-M = 2; %Size of feature vectors
-index = 3; %Which piece of data in each label to classify/test
+M = 6; %Size of feature vectors
+%index = 3; %Which piece of data in each label to classify/test
 
 clearvars training_data training_classes;
 
@@ -44,102 +44,111 @@ if exist('f') == 0,
     end
 end
 
+class1 = [];
+class2 = [];
+class3 = [];
 
-%Create 3 sets of data for each label
-class_data1 = [];
-class_data2 = [];
-class_data3 = [];
-for i=1:N,
-    if isequal(classes(i), 1),
-        class_data1(end+1,:) = vecs(i,:);
-    elseif isequal(classes(i), 2),
-        class_data2(end+1,:) = vecs(i,:);
-    elseif isequal(classes(i), 3),
-        class_data3(end+1,:) = vecs(i,:);
-    end
-end
-
-size1 = size(class_data1);
-size2 = size(class_data2);
-size3 = size(class_data3);
-
-%Create arrays of training data and test data
-clearvars temp_index; %If index is higher than number of data for a label, test the last set of data for that label
-if index > size1(1),
-    temp_index = size1(1);
-else
-    temp_index = index;
-end
-
-training_data = zeros(N-3, M);
-classify_data = zeros(3, M);
-counter = 1;
-
-for i=1:size1(1),
-    if i == temp_index,
-        classify_data(1,:) = class_data1(i,:,:);
-        continue;
-    end
-    training_data(counter,:) = class_data1(i,:,:);
-    counter = counter+1;
-end
-
-clearvars temp_index;
-if index > size2(1),
-    temp_index = size2(1);
-else
-    temp_index = index;
-end
-
-for i=1:size2(1),
-    if i == temp_index,
-        classify_data(2,:) = class_data2(i,:,:);
-        continue;
-    end
-    training_data(counter,:) = class_data2(i,:,:);
-    counter = counter+1;
-end
-
-clearvars temp_index;
-if index > size3(1),
-    temp_index = size3(1);
-else
-    temp_index = index;
-end
-
-for i=1:size3(1),
-    if i == temp_index,
-        classify_data(3,:) = class_data3(i,:,:);
-        continue;
-    end
-    training_data(counter,:) = class_data3(i,:,:);
-    counter = counter+1;
-end
-
-%Create training data, excluding one data of each label
-%training_data = [class_data1(1:end-1,:,:); class_data2(1:end-1,:,:); class_data3(1:end-1,:,:)];
-s = size(training_data);
-
-
-%Create corresponding labels array and descriptors array
-if exist('training_classes') == 0,
-    for i=1:s(1),
-        if i < size1(1),
-            training_classes(i) = 1;
-        elseif i < ((size1(1)-1) + size2(1)),
-            training_classes(i) = 2;
-        else,
-            training_classes(i) = 3;
+for index=1:8,
+    %Create 3 sets of data for each label
+    class_data1 = [];
+    class_data2 = [];
+    class_data3 = [];
+    for i=1:N,
+        if isequal(classes(i), 1),
+            class_data1(end+1,:) = vecs(i,:);
+        elseif isequal(classes(i), 2),
+            class_data2(end+1,:) = vecs(i,:);
+        elseif isequal(classes(i), 3),
+            class_data3(end+1,:) = vecs(i,:);
         end
     end
+
+    size1 = size(class_data1);
+    size2 = size(class_data2);
+    size3 = size(class_data3);
+
+    %Create arrays of training data and test data
+    clearvars temp_index; %If index is higher than number of data for a label, test the last set of data for that label
+    if index > size1(1),
+        temp_index = size1(1);
+    else
+        temp_index = index;
+    end
+
+    training_data = zeros(N-3, M);
+    classify_data = zeros(3, M);
+    counter = 1;
+
+    for i=1:size1(1),
+        if i == temp_index,
+            classify_data(1,:) = class_data1(i,:,:);
+            continue;
+        end
+        training_data(counter,:) = class_data1(i,:,:);
+        counter = counter+1;
+    end
+
+    clearvars temp_index;
+    if index > size2(1),
+        temp_index = size2(1);
+    else
+        temp_index = index;
+    end
+
+    for i=1:size2(1),
+        if i == temp_index,
+            classify_data(2,:) = class_data2(i,:,:);
+            continue;
+        end
+        training_data(counter,:) = class_data2(i,:,:);
+        counter = counter+1;
+    end
+
+    clearvars temp_index;
+    if index > size3(1),
+        temp_index = size3(1);
+    else
+        temp_index = index;
+    end
+
+    for i=1:size3(1),
+        if i == temp_index,
+            classify_data(3,:) = class_data3(i,:,:);
+            continue;
+        end
+        training_data(counter,:) = class_data3(i,:,:);
+        counter = counter+1;
+    end
+
+    %Create training data, excluding one data of each label
+    %training_data = [class_data1(1:end-1,:,:); class_data2(1:end-1,:,:); class_data3(1:end-1,:,:)];
+    s = size(training_data);
+
+
+    %Create corresponding labels array and descriptors array
+    if exist('training_classes') == 0,
+        for i=1:s(1),
+            if i < size1(1),
+                training_classes(i) = 1;
+            elseif i < ((size1(1)-1) + size2(1)),
+                training_classes(i) = 2;
+            else,
+                training_classes(i) = 3;
+            end
+        end
+    end
+
+    training_classes = training_classes'; %Transpose to one dimensional array
+
+    [means, invcors, aprioris] = BuildModel(M, training_data, s(1), 3, training_classes);
+
+
+    %Classify on last pieces of data
+    class1(end+1) = Classify(classify_data(1), 3, means, invcors, M, aprioris);
+    class2(end+1) = Classify(classify_data(2), 3, means, invcors, M, aprioris);
+    class3(end+1) = Classify(classify_data(3), 3, means, invcors, M, aprioris);
 end
 
-training_classes = training_classes'; %Transpose to one dimensional array
-
-[means, invcors, aprioris] = BuildModel(M, training_data, s(1), 3, training_classes);
-
-
-%Classify on last pieces of data
-class1 = Classify(classify_data(1), 3, means, invcors, M, aprioris);
-class2 = Classify(classify_data(2), 3, means, invcors, M, aprioris);
-class3 = Classify(classify_data(3), 3, means, invcors, M, aprioris);
+mean(class1)
+mean(class2)
+mean(class3)
