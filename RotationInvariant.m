0c7a351d615470b4a1ef1206c8753c6f529d2e6e
Andy <zegg90@gmail.com>
2012-03-08 16:14:08 +0000

    Fixed bug in descriptors

--- RotationInvariant.m
+++ RotationInvariant.m
@@ -10,7 +10,7 @@ for x=1:width,
     for y=1:height,
         c1 = complex(x - cr, y - cc);
         c2 = complex(x - cr, cc - y);
-        result = result + c1^u * c2^v;
+        result = result + c1^u * c2^v * double(mhi(x, y) / 255.0);
     end
 end
 
